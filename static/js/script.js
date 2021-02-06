6ebfc2633d0de8b05062938c4640e5af76d708f2
goodguytim1 <timobrien56@knights.ucf.edu>
2021-02-06 17:44:19 -0500

    Challenge 3: Rock, Paper, Scissors
    Use of cool JS objects

--- static/js/script.js
+++ static/js/script.js
@@ -15,9 +15,101 @@ function reset() {
 
 }
 
+// Challenge 2: Cat Generator
+
 function generateCat() {
     var image = document.createElement('img');
     var div = document.getElementById('flexCatGen');
     image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
     div.appendChild(image);
+}
+
+// Challenge 3: Rock, Paper, Scissors
+
+function rpsGame(yourChoice) {
+    // if(yourChoice.id == "rock") {
+    //     console.log("ROCK");
+    // }
+    var humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = randToRps();
+    results = decideWinner(humanChoice, botChoice); // [1, 0] or [0.5, 0.5] or [0, 1]
+    message = finalMessage(results); // {'message': 'You won!', 'color': 'green'}
+    console.log(message);
+    rpsFrontEnd(humanChoice, botChoice, message);
+}
+
+function randToRps() {
+    return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+}
+
+function decideWinner(hChoice, bChoice) {
+    // My if else solution
+    // if(hChoice == bChoice) {
+    //     return [.5, .5];
+    // } else if (hChoice == "rock") {
+    //     if (bChoice == "paper") {
+    //         return [0, 1];
+    //     } else {
+    //         return [1, 0];
+    //     }
+    // } else if (hChoice == "paper") {
+    //     if (bChoice == "scissors") {
+    //         return [0, 1];
+    //     } else {
+    //         return [1, 0];
+    //     }
+    // } else if (hChoice == "scissors") {
+    //     if (bChoice == "rock") {
+    //         return [0, 1];
+    //     } else {
+    //         return [1, 0];
+    //     }
+    // } else {
+    //     return [.5, .5];
+    // }
+
+    var rpsDataStructure = {
+        'rock': {'scissors': 1, 'rock': .5, 'paper': 0},
+        'paper': {'scissors': 0, 'rock': 1, 'paper': .5},
+        'scissors': {'scissors': .5, 'rock': 0, 'paper': 1}
+    };
+
+    var hScore = rpsDataStructure[hChoice][bChoice];
+    var bScore = rpsDataStructure[bChoice][hChoice];
+    return [hScore, bScore];
+    
+}
+
+function finalMessage ([hScore, bScore]) {
+    if (hScore === 0) {
+        return {'message': 'You lost!', 'color': 'red'};
+    } else if (hScore === .5) {
+        return {'message': 'You tied!', 'color': 'yellow'};
+    } else {
+        return {'message': 'You won!', 'color': 'green'};
+    }
+}
+
+function rpsFrontEnd (humanImageChoice, botImageChoice, finalMessage) {
+    var imagesDB = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src
+    };
+
+    document.getElementById('rock').remove();
+    document.getElementById('scissors').remove();
+    document.getElementById('paper').remove();
+
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='" + imagesDB[humanImageChoice] + "' height= '150px' width= '150px' style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1)'>";
+    botDiv.innerHTML = "<img src='" + imagesDB[botImageChoice] + "' height= '150px' width= '150px' style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1)'>";
+    messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>";
+    document.getElementById("flex-box-rps-div").appendChild(humanDiv);
+    document.getElementById("flex-box-rps-div").appendChild(messageDiv);
+    document.getElementById("flex-box-rps-div").appendChild(botDiv);
 }
